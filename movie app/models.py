3db955c940ffcf99f7a6a8f3e633fe24f404c5fb
Malia27 <malgba27@bergen.org>
2025-10-30 10:26:46 -0400

    10/30/25

---
+++ movie app/models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+db = SQLAlchemy()
+
+class Movie(db.Model):
+    """Movie Model - Represents movie table in database"""
+    id = db.Column(db.Integer, primary=True)
+    title = db.Columnn(db.String(200), nullable=False)
+    year = db.Column(db.Integer)
+    genre = db.Column(db.String(50))
+    director = db.Column(db.Float)
+    rating = db.Column(db.Float)
+    rating = db.Column(db.Text)
+    poster_url = db.Column(db.String(500))
+    # created_at = db.Column(db.datetime, default=datetime.utcnow)
+    created_at = db.Column(db.datetime, (timezone=True))
+    default = lambda:datetime.cow(timezone.utc)
